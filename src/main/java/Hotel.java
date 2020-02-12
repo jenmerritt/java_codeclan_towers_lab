1b75505798edd0ecd40a8ce55ccf5ce99ff6becc
Jen Merritt <jenjomez@gmail.com>
2020-02-12 11:17:08 +0000

    setup hotel class

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,12 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
 public class Hotel {
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel(){
+        this.bedrooms = new ArrayList<Bedroom>();
+        this.conferenceRooms = new ArrayList<ConferenceRoom>();
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,11 @@
+import org.junit.Before;
+
 public class HotelTest {
+
+    private Hotel theNorth;
+
+    @Before
+    public void before(){
+        theNorth = new Hotel();
+    }
 }
